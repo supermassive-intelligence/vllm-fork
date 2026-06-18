13eb641fa2070b1bea9383551bd1d65e03234a84
Greg Diamos <gregory.diamos@gmail.com>
2026-06-18 01:41:52 -0700

    updated metrics
    (cherry picked from commit 23fa3ecf8abdca8106b04b635ba1345ff5d2a973)

--- vllm/entrypoints/serve/instrumentator/metrics.py
+++ vllm/entrypoints/serve/instrumentator/metrics.py
@@ -15,9 +15,11 @@ from vllm.v1.metrics.prometheus import get_prometheus_registry
 
 # Workaround for AttributeError: '_IncludedRouter' object has no attribute 'path'
 # in prometheus-fastapi-instrumentator when using nested routers.
+_original_get_route_name = _pfi_routing.get_route_name
+
 def patched_get_route_name(request):
     try:
-        return _pfi_routing.get_route_name(request)
+        return _original_get_route_name(request)
     except AttributeError:
         return "unknown_route"
 
