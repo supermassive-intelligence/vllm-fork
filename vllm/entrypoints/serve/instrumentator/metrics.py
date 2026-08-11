vllm-project/vllm

patch to metrics

vllm/entrypoints/serve/instrumentator/metrics.py
@@ -6,10 +6,20 @@ import prometheus_client
 import regex as re
 from fastapi import FastAPI, Response
 from prometheus_client import make_asgi_app
-from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator, routing as instrumentator_routing
 from starlette.routing import Mount
 
 from vllm.v1.metrics.prometheus import get_prometheus_registry
+
+# Workaround for AttributeError: '_IncludedRouter' object has no attribute 'path'
+# in prometheus-fastapi-instrumentator when using nested routers.
+def patched_get_route_name(request):
+    try:
+        return instrumentator_routing.get_route_name(request)
+    except AttributeError:
+        return "unknown_route"
+
+instrumentator_routing.get_route_name = patched_get_route_name
 
 
 class PrometheusResponse(Response):

supermassive-intelligence/vllm-fork

(cherry picked from commit 15f729a1ab3dac335a037f1554a548b7c3b4fca4)

vllm/entrypoints/serve/instrumentator/metrics.py
@@ -13,6 +13,16 @@ from starlette.types import Scope
 
 from vllm.v1.metrics.prometheus import get_prometheus_registry
 
+# Workaround for AttributeError: '_IncludedRouter' object has no attribute 'path'
+# in prometheus-fastapi-instrumentator when using nested routers.
+def patched_get_route_name(request):
+    try:
+        return _pfi_routing.get_route_name(request)
+    except AttributeError:
+        return "unknown_route"
+
+_pfi_routing.get_route_name = patched_get_route_name
+
 
 def _patch_instrumentator_route_walk() -> None:
     """Make prometheus-fastapi-instrumentator's route walk tolerate routes